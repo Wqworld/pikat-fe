fa0dc7b738bbdc8005e8a54e49dc2d06e60169a1
Waqqir Humaid <waqqirhumaid1@gmail.com>
2025-12-15 21:20:07 +0700

    login dan get data

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,26 +1,12 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
-
 const api = axios.create({
-  baseURL: BASE_URL,
+  baseURL: "https://piket-nekat-be.vercel.app/", 
+  withCredentials: true, 
+  
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true, 
 });
-
-api.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      if (typeof window !== "undefined") {
-        console.error("Sesi habis! Mengalihkan ke login...");
-        window.location.href = "/login"; 
-      }
-    }
-    return Promise.reject(error);
-  }
-);
 
 export default api;
